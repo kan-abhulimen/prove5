fc84fb691c2b6003f7fcf310e13d7278b9c15154
Kan Abhulimen <ehi12001@byui.edu>
2017-05-20 14:07:06 -0600

    Initial commit

---
+++ app/src/main/java/com/helpingfeets/prove5/favActivity.java
@@ -0,0 +1,13 @@
+package com.helpingfeets.prove5;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class favActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_fav);
+    }
+}
